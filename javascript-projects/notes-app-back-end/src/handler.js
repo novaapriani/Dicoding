474251a8204c27dc67fcac08a1dc863d5a73ae35
novaapriani <novaapriani531@gmail.com>
2023-03-04 01:12:43 +0700

    feat: add put data handler

--- javascript-projects/notes-app-back-end/src/handler.js
+++ javascript-projects/notes-app-back-end/src/handler.js
@@ -60,8 +60,42 @@ const getNoteByIdHandler = (req, h) => {
   return response;
 };
 
+const editNoteByIdHandler = (req, h) => {
+  const { id } = req.params;
+  const { title, tags, body } = req.payload;
+  const updatedAt = new Date().toISOString;
+
+  const index = notes.findIndex((note) => note.id === id);
+
+  if (index !== -1) {
+    notes[index] = {
+      ...notes[index],
+      title,
+      tags,
+      body,
+      updatedAt,
+    };
+
+    const response = h.response({
+      status: 'success',
+      message: 'catatan berhasil diperbarui',
+    });
+
+    response.code(200);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Gagal memperbarui catatan. Id tidak ditemukan!',
+  });
+  response.code(404);
+  return response;
+};
+
 module.exports = {
   addNoteHandler,
   getAllNotesHandler,
   getNoteByIdHandler,
+  editNoteByIdHandler,
 };
