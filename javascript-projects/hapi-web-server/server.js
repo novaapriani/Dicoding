b916e5a6f08351789cfc037af085c29c770223ad
novaapriani <novaapriani531@gmail.com>
2023-03-03 15:45:32 +0700

    fix: update route

--- javascript-projects/hapi-web-server/server.js
+++ javascript-projects/hapi-web-server/server.js
@@ -1,4 +1,5 @@
 const Hapi = require("@hapi/hapi")
+const routes = require("./routes")
 
 const init = async () => {
   // Hapi.server => HTTP server
@@ -6,6 +7,8 @@ const init = async () => {
     port: 5000,
     host: "localhost",
   })
+
+  server.route(routes)
 
   await server.start()
   //   alamat lengkap server di-running
