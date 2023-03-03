c3aa90f40e568f835ba2913d8e0435ed81940c37
novaapriani <novaapriani531@gmail.com>
2023-03-03 12:21:54 +0700

    feat: add response status

--- javascript-projects/nodejs-web-server/server.js
+++ javascript-projects/nodejs-web-server/server.js
@@ -5,26 +5,42 @@ const host = "localhost"
 
 const requestListener = (req, res) => {
   // method apa yg dipakai
-  const { method } = req
+  const { method, url } = req
 
-  if (method === "GET") {
-    res.setHeader("Content-Type", "text/html")
+  if (url === "/") {
+    if (method === "GET") {
+      res.setHeader("Content-Type", "text/html")
 
-    res.statusCode = 200
-    res.end("<h1>Halo HTTP server!</h1>")
-  }
+      res.statusCode = 200
+      res.end("<h1>Ini adalah homepage!</h1>")
+    } else {
+      res.statusCode = 400
+      res.end(`Halaman tidak dapat diakses dengan ${method} request!`)
+    }
+  } else if (url === "/about") {
+    if (method === "GET") {
+      res.setHeader("Content-Type", "text/html")
 
-  if (method === "POST") {
-    let body = []
+      res.statusCode = 200
+      res.end("<h1>Halo ini adalah halaman About!</h1>")
+    } else if (method === "POST") {
+      let body = []
 
-    req.on("data", (chunk) => body.push(chunk))
-    req.on("end", () => {
-      // ubah buffer menjadi actual data
-      body = Buffer.concat(body).toString()
-      //   parse data yg dikirim
-      const { name } = JSON.parse(body)
-      res.end(`Hai ${name}`)
-    })
+      req.on("data", (chunk) => body.push(chunk))
+      req.on("end", () => {
+        // ubah buffer menjadi actual data
+        body = Buffer.concat(body).toString()
+        //   parse data yg dikirim
+        const { name } = JSON.parse(body)
+        res.end(`Halo ${name}! Ini adalah halaman about.`)
+      })
+    } else {
+      res.statusCode = 400
+      res.end(`Halaman tidak dapat diakses dengan ${method} request!`)
+    }
+  } else {
+    res.statusCode = 404
+    res.end("Halaman tidak dapat ditemukan!")
   }
 }
 
