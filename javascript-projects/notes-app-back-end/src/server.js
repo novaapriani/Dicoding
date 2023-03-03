db07255976dd6f6dc08cca69ce7d97025db7f469
novaapriani <novaapriani531@gmail.com>
2023-03-03 20:55:20 +0700

    feat: add route to server

--- javascript-projects/notes-app-back-end/src/server.js
+++ javascript-projects/notes-app-back-end/src/server.js
@@ -1,4 +1,5 @@
 const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
 
 const init = async () => {
   const server = Hapi.server({
@@ -6,6 +7,7 @@ const init = async () => {
     host: 'localhost',
   });
 
+  server.route(routes);
   await server.start();
   console.log(`Server berjalan di ${server.info.uri}`);
 };
