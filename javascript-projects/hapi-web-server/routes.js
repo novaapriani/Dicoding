ebbd2ea126c68334eee4abe502c5e7f895ead6d3
novaapriani <novaapriani531@gmail.com>
2023-03-03 15:46:39 +0700

    feat: add routes

---
+++ javascript-projects/hapi-web-server/routes.js
@@ -0,0 +1,41 @@
+const routes = [
+  {
+    method: "GET",
+    path: "/",
+    handler: (req, h) => {
+      return "Homepage"
+    },
+  },
+  {
+    method: "*",
+    path: "/",
+    handler: (req, h) => {
+      return "Halaman tidak dapat diakses dengan method tersebut"
+    },
+  },
+  {
+    method: "GET",
+    path: "/about",
+    handler: (req, h) => {
+      return "About Page"
+    },
+  },
+  {
+    method: "*",
+    path: "/about",
+    handler: (req, h) => {
+      return "Halaman tidak dapat diakses dengan method tersebut"
+    },
+  },
+  {
+    // prevent method yg belum di-define disetiap route
+    method: "*",
+    // menangani path yg belum ditentukan
+    path: "/{any*}",
+    handler: (req, h) => {
+      return "Halaman tidak dapat ditemukan"
+    },
+  },
+]
+
+module.exports = routes
