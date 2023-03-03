d8c617f0be2094b8b4f3831d7ad610642aa6cf61
novaapriani <novaapriani531@gmail.com>
2023-03-04 00:53:22 +0700

    feat: add get data handler

--- javascript-projects/notes-app-back-end/src/handler.js
+++ javascript-projects/notes-app-back-end/src/handler.js
@@ -1,7 +1,7 @@
 const { nanoid } = require('nanoid');
 const notes = require('./notes');
 
-const addNoteHandlers = (req, h) => {
+const addNoteHandler = (req, h) => {
   const id = nanoid(16);
   const { title, tags, body } = req.payload;
   const createdAt = new Date().toISOString();
@@ -21,10 +21,47 @@ const addNoteHandlers = (req, h) => {
     });
 
     response.code(201);
+    //   .header(
+    //     'Access-Control-Allow-Origin',
+    //     'http://notesapp-v1.dicodingacademy.com',
+    //   );
+    //   data dikonsumsi di seluruh origin
+    //   .header('Access-Control-Allow-Origin', '*');
     return response;
   }
 };
 
+const getAllNotesHandler = () => ({
+  status: 'success',
+  data: {
+    notes,
+  },
+});
+
+const getNoteByIdHandler = (req, h) => {
+  const { id } = req.params;
+
+  const note = notes.filter((n) => n.id === id)[0];
+
+  if (note !== undefined) {
+    return {
+      status: 'success',
+      data: {
+        note,
+      },
+    };
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'catatan tidak ditemukan',
+  });
+  response.code(404);
+  return response;
+};
+
 module.exports = {
-  addNoteHandlers,
+  addNoteHandler,
+  getAllNotesHandler,
+  getNoteByIdHandler,
 };
