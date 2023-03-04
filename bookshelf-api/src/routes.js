0ed3f1d9d7078c9bc4b2f5aa9622be276acea33d
novaapriani <novaapriani531@gmail.com>
2023-03-04 15:17:12 +0700

    feat: add post route

---
+++ bookshelf-api/src/routes.js
@@ -0,0 +1,11 @@
+const { addBookHandler } = require('./handlers');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBookHandler,
+  },
+];
+
+module.exports = routes;
