c2b2a44dceebdf4022a96503f5e5a380898807c4
novaapriani <novaapriani531@gmail.com>
2023-03-04 15:35:24 +0700

    feat: add get all router

--- bookshelf-api/src/routes.js
+++ bookshelf-api/src/routes.js
@@ -1,10 +1,15 @@
-const { addBookHandler } = require('./handlers');
+const { addBookHandler, getAllBooksHandler } = require('./handlers');
 
 const routes = [
   {
     method: 'POST',
     path: '/books',
     handler: addBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler,
   },
 ];
 
