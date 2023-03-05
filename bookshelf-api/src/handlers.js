1b7624a5c0f8967071b5269c41986b608b72a5a6
novaapriani <novaapriani531@gmail.com>
2023-03-05 16:15:25 +0700

    feat: add delete handler

--- bookshelf-api/src/handlers.js
+++ bookshelf-api/src/handlers.js
@@ -145,9 +145,37 @@ const updateBookByIdHandler = (req, h) => {
   return response;
 };
 
+const deleteBookByIdHandler = (req, h) => {
+  const { bookId } = req.params;
+  const { id } = books;
+
+  const book = books.findIndex((b) => b.id === bookId);
+
+  if (book !== -1) {
+    books.splice(book, 1);
+
+    const response = h.response({
+      status: 'success',
+      message: 'Buku berhasil dihapus',
+    });
+
+    response.code(200);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Buku gagal dihapus. Id tidak ditemukan',
+  });
+
+  response.code(404);
+  return response;
+};
+
 module.exports = {
   addBookHandler,
   getAllBooksHandler,
   getBookByIdHandler,
   updateBookByIdHandler,
+  deleteBookByIdHandler,
 };
