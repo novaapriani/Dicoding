d34afbaf82fcd8d2f6e9d943bbbb0ce99a34877d
novaapriani <novaapriani531@gmail.com>
2023-03-05 16:01:20 +0700

    feat: add put data handler

--- bookshelf-api/src/handlers.js
+++ bookshelf-api/src/handlers.js
@@ -94,8 +94,60 @@ const getBookByIdHandler = (req, h) => {
   return response;
 };
 
+const updateBookByIdHandler = (req, h) => {
+  const { bookId } = req.params;
+  const { id } = books;
+
+  const {
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading,
+  } = req.payload;
+
+  if (!name) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal memperbarui buku. Mohon isi nama buku',
+    });
+
+    response.code(400);
+    return response;
+  } else if (readPage > pageCount) {
+    const response = h.response({
+      status: 'fail',
+      message:
+        'Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount',
+    });
+
+    response.code(400);
+    return response;
+  } else if (bookId !== id) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal memperbarui buku. Id tidak ditemukan',
+    });
+
+    response.code(404);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'success',
+    message: 'Buku berhasil diperbarui',
+  });
+
+  response.code(200);
+  return response;
+};
+
 module.exports = {
   addBookHandler,
   getAllBooksHandler,
   getBookByIdHandler,
+  updateBookByIdHandler,
 };
