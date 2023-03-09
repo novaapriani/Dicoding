a07e3fa7018a8e3a42f02b51785534726f1e76d9
novaapriani <novaapriani531@gmail.com>
2023-03-10 01:32:57 +0700

    fix: Pool import

--- notes-app-back-end-plugin/src/services/postgres/UsersService.js
+++ notes-app-back-end-plugin/src/services/postgres/UsersService.js
@@ -1,4 +1,4 @@
-const Pool = require('pg');
+const { Pool } = require('pg');
 const nanoid = require('nanoid');
 const bcrypt = require('bcrypt');
 const InvariantError = require('../../exceptions/InvariantError');
