b4a32da74067bc9ac44b48628e48165a0bd1dbf2
novaapriani <novaapriani531@gmail.com>
2023-03-15 18:57:17 +0700

    chore: delete unnecessary code

--- open-music-back-end/src/api/exports/handler.js
+++ open-music-back-end/src/api/exports/handler.js
@@ -1,5 +1,4 @@
 const autoBind = require('auto-bind');
-const ClientError = require('../../exceptions/ClientError');
 
 class ExportsHandler {
   constructor(service, validator, playlistsService) {
@@ -11,53 +10,27 @@ class ExportsHandler {
   }
 
   async postExportPlaylistByIdHandler(req, h) {
-    try {
-      this._validator.validateExportPlaylistByIdPayload(req.payload);
-      const { id: playlistId } = req.params;
-      const { id: credentialId } = req.auth.credentials;
+    this._validator.validateExportPlaylistByIdPayload(req.payload);
+    const { id: playlistId } = req.params;
+    const { id: credentialId } = req.auth.credentials;
 
-      await this._playlistsService.verifyPlaylistOwner(
-        playlistId,
-        credentialId,
-      );
-      await this._playlistsService.verifyPlaylist(playlistId);
+    await this._playlistsService.verifyPlaylistOwner(playlistId, credentialId);
+    await this._playlistsService.verifyPlaylist(playlistId);
 
-      const message = {
-        userId: req.auth.credentials.id,
-        playlistId,
-        targetEmail: req.payload.targetEmail,
-      };
+    const message = {
+      userId: req.auth.credentials.id,
+      playlistId,
+      targetEmail: req.payload.targetEmail,
+    };
 
-      await this._service.sendMessage(
-        'export:playlist',
-        JSON.stringify(message),
-      );
+    await this._service.sendMessage('export:playlist', JSON.stringify(message));
 
-      const response = h.response({
+    return h
+      .response({
         status: 'success',
         message: 'Permintaan Anda dalam antrean',
-      });
-      response.code(201);
-      return response;
-    } catch (error) {
-      if (error instanceof ClientError) {
-        const response = h.response({
-          status: 'fail',
-          message: error.message,
-        });
-        response.code(error.statusCode);
-        return response;
-      }
-
-      // Server ERROR!
-      const response = h.response({
-        status: 'error',
-        message: 'Maaf, terjadi kegagalan pada server kami.',
-      });
-      response.code(500);
-      console.error(error);
-      return response;
-    }
+      })
+      .code(201);
   }
 }
 
--- open-music-back-end/src/api/uploads/handler.js
+++ open-music-back-end/src/api/uploads/handler.js
@@ -1,6 +1,5 @@
 const autoBind = require('auto-bind');
 const config = require('../../utils/config');
-const ClientError = require('../../exceptions/ClientError');
 
 class UploadsHandler {
   constructor(service, albumsService, validator) {
@@ -12,42 +11,22 @@ class UploadsHandler {
   }
 
   async postAlbumCoverHandler(req, h) {
-    try {
-      const { cover } = req.payload;
-      const { id } = req.params;
+    const { cover } = req.payload;
+    const { id } = req.params;
 
-      this._validator.validateImageHeaders(cover.hapi.headers);
+    this._validator.validateImageHeaders(cover.hapi.headers);
 
-      const filename = await this._service.writeFile(cover, cover.hapi);
-      const fileLocation = `http://${config.app.host}:5000/upload/images/${filename}`;
+    const filename = await this._service.writeFile(cover, cover.hapi);
+    const fileLocation = `http://${config.app.host}:5000/upload/images/${filename}`;
 
-      await this._albumsService.addCoverAlbum(id, fileLocation);
+    await this._albumsService.addCoverAlbum(id, fileLocation);
 
-      return h
-        .response({
-          status: 'success',
-          message: 'Cover berhasil ditambah',
-        })
-        .code(201);
-    } catch (error) {
-      if (error instanceof ClientError) {
-        const response = h.response({
-          status: 'fail',
-          message: error.message,
-        });
-        response.code(error.statusCode);
-        return response;
-      }
-
-      // Server ERROR!
-      const response = h.response({
-        status: 'error',
-        message: 'Maaf, terjadi kegagalan pada server kami.',
-      });
-      response.code(500);
-      console.error(error);
-      return response;
-    }
+    return h
+      .response({
+        status: 'success',
+        message: 'Cover berhasil ditambah',
+      })
+      .code(201);
   }
 }
 
